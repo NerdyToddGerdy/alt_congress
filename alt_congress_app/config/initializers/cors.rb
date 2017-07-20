ff8cf881311eeb966daee9ed956185113fd57495
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-07-20 16:53:04 -0400

    http S

--- alt_congress_app/config/initializers/cors.rb
+++ alt_congress_app/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://alt-congress.herokuapp.com'
+    origins 'https://alt-congress.herokuapp.com'
 
     resource '*',
       headers: :any,
