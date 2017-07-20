af1a4d09f6566ab22c076219bb95cdd77ec25e92
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-07-20 16:33:59 -0400

    added create to the users controller

--- alt_congress_app/app/controllers/users_controller.rb
+++ alt_congress_app/app/controllers/users_controller.rb
@@ -7,4 +7,17 @@ class UsersController < ApplicationController
         user = User.find(params[:id])
         render json: {status: 200, user: user}
     end
+    def create
+        user = User.new(user_params)
+        
+        if user.save
+            render json: user, status: :created, location: user
+        else
+            render json: user, status: :unprocessable_entity
+        end
+    end
+    private
+        def user_params
+           params.require(:user).permit(:first_name, :last_name, :email, :address, :State, :district, :gender, :i_fund, :i_join, :i_vote, :verified, :zip)
+        end
 end
