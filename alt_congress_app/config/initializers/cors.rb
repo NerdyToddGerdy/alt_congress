303ba3fe910e3aac43ee6b971f25c5b14bcc26ef
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-07-20 16:50:49 -0400

    changed the origins in cors

--- alt_congress_app/config/initializers/cors.rb
+++ alt_congress_app/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'alt-congress.herokuapp.com'
+    origins 'http://alt-congress.herokuapp.com'
 
     resource '*',
       headers: :any,
