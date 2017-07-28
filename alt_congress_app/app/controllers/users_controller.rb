8a894f690fa83ede9129c266354b9d93249c9927
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-07-28 16:44:52 -0400

    added city to users table

--- alt_congress_app/app/controllers/users_controller.rb
+++ alt_congress_app/app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
     end
     private
         def user_params
-           params.require(:user).permit(:first_name, :last_name, :email, :address, :State, :district, :gender, :i_fund, :i_join, :i_vote, :verified, :zip)
+           params.require(:user).permit(:first_name, :last_name, :email, :address, :State, :district, :gender, :i_fund, :i_join, :i_vote, :verified, :zip, :city)
         end
 end
--- alt_congress_app/db/schema.rb
+++ alt_congress_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728175447) do
+ActiveRecord::Schema.define(version: 20170728204157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20170728175447) do
     t.boolean "i_fund"
     t.boolean "i_join"
     t.boolean "verified"
+    t.string  "city"
   end
 
 end
